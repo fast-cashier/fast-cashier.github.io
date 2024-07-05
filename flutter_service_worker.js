9d70ef7957951ba0f7bb218920c329ec09197b24
baniramdlan <baniramdlan@gmail.com>
2024-07-05 17:59:32 +0700

    V.2.6

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7fc0f40bc0085de88024711f98cec877
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "1b68d2a6785b0f784053f850c2a61bfe",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "a62bf6a286f61f9dfe5017a4dd5aee8e",
+"flutter_bootstrap.js": "2c6c797714b3554ae7ffa0f32d8ad5dc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
